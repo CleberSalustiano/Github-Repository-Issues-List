fa30d08abbf5cab93faa4516833f8a4bd2dba0a8
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-05-06 20:28:30 -0300

    feat: github api integration completed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,61 +1,73 @@
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable react/function-component-definition */
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logoApp.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explore" />
-    <Title>Explore repositórios no Github</Title>
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars.githubusercontent.com/u/84328609?v=4"
-          alt="Cleber Salustiano"
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositores] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    /*
+     Adição de um novo repositório
+     Consumir API do github
+     Salvar novo repositório no  */
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositores([...repositories, repository]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explore" />
+      <Title>Explore repositórios no Github</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>github/clebersalustiano</strong>
-          <p>Esse aqui é o meu repositório</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-      <a href="teste">
-        <img
-          src="https://avatars.githubusercontent.com/u/84328609?v=4"
-          alt="Cleber Salustiano"
-        />
-        <div>
-          <strong>github/clebersalustiano</strong>
-          <p>Esse aqui é o meu repositório</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-
-      <a href="teste">
-        <img
-          src="https://avatars.githubusercontent.com/u/84328609?v=4"
-          alt="Cleber Salustiano"
-        />
-        <div>
-          <strong>github/clebersalustiano</strong>
-          <p>Esse aqui é o meu repositório</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
