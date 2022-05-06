df6a6eb1a49e1cf0d1f90dac60f6e691f5901f3a
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-05-06 19:27:16 -0300

    feat: dashboard style reference completed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logoApp.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explore" />
+    <Title>Explore repositórios no Github</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/84328609?v=4"
+          alt="Cleber Salustiano"
+        />
+        <div>
+          <strong>github/clebersalustiano</strong>
+          <p>Esse aqui é o meu repositório</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/84328609?v=4"
+          alt="Cleber Salustiano"
+        />
+        <div>
+          <strong>github/clebersalustiano</strong>
+          <p>Esse aqui é o meu repositório</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars.githubusercontent.com/u/84328609?v=4"
+          alt="Cleber Salustiano"
+        />
+        <div>
+          <strong>github/clebersalustiano</strong>
+          <p>Esse aqui é o meu repositório</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
