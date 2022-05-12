e10dcf351a87a8c1d275d589fba78fe4b321a00c
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-05-12 20:54:53 -0300

    feat: static style completed

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,11 +1,61 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logoApp.svg';
 
 const Repository: React.FC = () => {
   const { user, repositoryName } = useParams();
   const repository = `${String(user)}/${String(repositoryName)}`;
 
-  return <h1>Repository:{repository} </h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Exporer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/84328609?v=4"
+            alt="img"
+          />
+          <div>
+            <strong>cleber/teste</strong>
+            <p>Lorem ipsum, dolor sit amet consectetur</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="s">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 export default Repository;
